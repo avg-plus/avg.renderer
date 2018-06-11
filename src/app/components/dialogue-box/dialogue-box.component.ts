7b80826fb2517823d8dc8d003bd153d8c2f5e418
AngryPowman <138001655@qq.com>
2018-06-12 00:12:05 +0800

    Init Live2D models

--- src/app/components/dialogue-box/dialogue-box.component.ts
+++ src/app/components/dialogue-box/dialogue-box.component.ts
@@ -25,8 +25,8 @@ import * as gsap from "gsap";
 import { Subject } from "rxjs/Subject";
 import { Observable } from "rxjs/Observable";
 
-// import "app/common/live2d/lib/live2d.min.js";
-// import "pixi-live2d";
+import "app/common/live2d/lib/live2d.min.js";
+import "pixi-live2d";
 
 // import * as gsap from 'gsap';
 
@@ -122,41 +122,41 @@ export class DialogueBoxComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngAfterViewInit() {
-    // const renderer = new PIXI.WebGLRenderer(800, 600);
-    // document.getElementById("#character-box").appendChild(renderer.view);
-    // const stage = new PIXI.Container();
-    // const modelHaru = require("app/common/live2d/sample/sampleApp1/assets/live2d/shizuku/shizuku.model.json");
-    // const sprite = new PIXI.Sprite(); // PIXI.Sprite.fromImage('assets/graphics/characters/live2d/7_room2_a.jpg');
-    // stage.addChild(sprite);
-    // const live2dSprite = new PIXI["Live2DSprite"](modelHaru, {
-    //   debugLog: true,
-    //   randomMotion: true,
-    //   eyeBlink: true
-    //   // audioPlayer: (...args) => console.log(...args)
-    // });
-    // stage.addChild(live2dSprite);
+    const renderer = new PIXI.WebGLRenderer(800, 600, { transparent: true });
+    // document.getElementById("dialogue-container").appendChild(renderer.view);
+    const stage = new PIXI.Container();
+    const modelHaru = require("app/common/live2d/sample/sampleApp1/assets/live2d/shizuku/shizuku.model.json");
+    const sprite = new PIXI.Sprite(); // PIXI.Sprite.fromImage('assets/graphics/characters/live2d/7_room2_a.jpg');
+    stage.addChild(sprite);
+    const live2dSprite = new PIXI["Live2DSprite"](modelHaru, {
+      debugLog: true,
+      randomMotion: true,
+      eyeBlink: true
+      // audioPlayer: (...args) => console.log(...args)
+    });
+    stage.addChild(live2dSprite);
     // live2dSprite.x = -105;
-    // // live2dSprite.y = -150;
-    // live2dSprite.adjustScale(0, 0, 0.7);
-    // live2dSprite.adjustTranslate(0.4, 0);
-    // live2dSprite.startRandomMotion("idle");
-    // live2dSprite.on("click", evt => {
-    //   const point = evt.data.global;
-    //   if (live2dSprite.hitTest("body", point.x, point.y)) {
-    //     live2dSprite.startRandomMotionOnce("tap_body");
-    //   }
-    //   if (live2dSprite.hitTest("head", point.x, point.y)) {
-    //     live2dSprite.playSound("星のカケラ.mp3", "sound/");
-    //   }
-    // });
-    // live2dSprite.on("mousemove", evt => {
-    //   const point = evt.data.global;
-    //   live2dSprite.setViewPoint(point.x, point.y);
-    // });
-    // function animate() {
-    //   requestAnimationFrame(animate);
-    //   renderer.render(stage);
-    // }
+    // live2dSprite.y = -150;
+    live2dSprite.adjustScale(0, 0, 0.7);
+    live2dSprite.adjustTranslate(0.4, 0);
+    live2dSprite.startRandomMotion("idle");
+    live2dSprite.on("click", evt => {
+      const point = evt.data.global;
+      if (live2dSprite.hitTest("body", point.x, point.y)) {
+        live2dSprite.startRandomMotionOnce("tap_body");
+      }
+      if (live2dSprite.hitTest("head", point.x, point.y)) {
+        // live2dSprite.playSound("星のカケラ.mp3", "sound/");
+      }
+    });
+    live2dSprite.on("mousemove", evt => {
+      const point = evt.data.global;
+      live2dSprite.setViewPoint(point.x, point.y);
+    });
+    function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(stage);
+    }
     // animate();
   }
 
