8c1afd756688c756a1fd452570ef484d2257bd11
AngryPowman <138001655@qq.com>
2018-06-24 02:28:45 +0800

    Add animateCssFilter to AnimationUtils

--- src/app/common/animations/animation-utils.ts
+++ src/app/common/animations/animation-utils.ts
@@ -1,25 +1,17 @@
 import * as gsap from "gsap";
+import * as $ from "jquery";
+import { EngineUtils } from "avg-engine/engine";
 
 export class AnimationUtils {
-  public static fadeTo(
-    target: string,
-    duration: number = 200,
-    to: number = 1,
-    complete?: () => void
-  ) {
+  public static fadeTo(target: string, duration: number = 200, to: number = 1, complete?: () => void) {
     AnimationUtils.to("FadeTo", target, duration, { opacity: to }, complete);
   }
 
-  public static moveTo(
-    target: string,
-    duration: number = 200,
-    x: number,
-    y: number,
-    complete?: () => void
-  ) {
+  public static moveTo(target: string, duration: number = 200, x: number, y: number, complete?: () => void) {
     AnimationUtils.to("MoveTo", target, duration, { x: x, y: y }, complete);
   }
 
+  // Set transform origin anchor of the target
   public static setAnchor(target: string, anchor: string = "center center") {
     AnimationUtils.to(
       "scaleTo",
@@ -31,13 +23,99 @@ export class AnimationUtils {
       () => {}
     );
   }
+  /**
+   *  animateCssFilter
+   *
+   * @static
+   * @param {string} target
+   * @param {string} filterProperty
+   * @param {number} normalizationValue: normalize to 1 - 100
+   * @returns
+   * @memberof AnimationUtils
+   */
+  public static animateCssFilter(target: string, filterProperty: string, duration: number, strength: number) {
+    const FILTERS = new Map([
+      ["blur", "px"],
+      ["brightness", "%"],
+      ["contrast", "%"],
+      ["grayscale", "%"],
+      ["hue-rotate", "deg"],
+      ["invert", "%"],
+      ["opacity", "%"],
+      ["saturate", "%"],
+      ["sepia", "%"]
+    ]);
 
-  public static scaleTo(
-    target: string,
-    duration: number = 200,
-    ratio: number,
-    complete?: () => void
-  ) {
+    FILTERS.forEach((v, k) => {
+      if (v === filterProperty) {
+        console.warn("Effect Name {0} not found.", filterProperty);
+        return;
+      }
+    });
+
+    let value = (strength || 0) * 1;
+    value = EngineUtils.NumericRange(value, 0, 100);
+
+    if (filterProperty === "hue-rotate") {
+      value = value * (360 / 100); // normalize to 360
+    } else if (filterProperty === "blur") {
+      value = value / 10; // blur max = 10px
+    }
+
+    const elementID = target;
+    const e = $(elementID);
+
+    let currentFilters = e.css("filter");
+    let filters = EngineUtils.parseCSSFilters(currentFilters);
+
+    // Get the value of current effectName
+    let currentEffectValue = filters.get(filterProperty);
+    if (currentEffectValue) {
+      if (filterProperty === "hue-rotate") {
+        currentEffectValue = currentEffectValue.replace("deg", "");
+      } else if (filterProperty === "blur") {
+        currentEffectValue = currentEffectValue.replace("px", "");
+      }
+    }
+
+    console.log("currentEffectValue", currentEffectValue);
+
+    let startValue = 0;
+    if (currentEffectValue) {
+      startValue = Number(currentEffectValue);
+    } else {
+      // opacity's initial value is 100%
+      if (filterProperty === "opacity" || filterProperty === "brightness") {
+        startValue = 100;
+      }
+    }
+
+    return new Promise((resolve, reject) => {
+      EngineUtils.countTo(
+        startValue,
+        value,
+        duration,
+        v => {
+          // console.log(v);
+          // Set exist filters
+          currentFilters = e.css("filter");
+          filters = EngineUtils.parseCSSFilters(currentFilters);
+          e.css("filter", currentFilters);
+
+          // Apply new filters value
+          const filterValue = v + FILTERS.get(filterProperty);
+          filters.set(filterProperty, filterValue);
+
+          const newFilters = EngineUtils.toCSSFilter(filters);
+
+          e.css("filter", newFilters);
+        },
+        resolve
+      );
+    });
+  }
+
+  public static scaleTo(target: string, duration: number = 200, ratio: number, complete?: () => void) {
     AnimationUtils.to(
       "scaleTo",
       target,
@@ -49,12 +127,7 @@ export class AnimationUtils {
     );
   }
 
-  public static rotateTo(
-    target: string,
-    duration: number = 200,
-    angle: number,
-    complete?: () => void
-  ) {
+  public static rotateTo(target: string, duration: number = 200, angle: number, complete?: () => void) {
     AnimationUtils.to(
       "rotateTo",
       target,
@@ -66,33 +139,24 @@ export class AnimationUtils {
     );
   }
 
-  public static to(
-    name: string,
-    target: string,
-    duration: number = 200,
-    vars: any,
-    complete?: () => void
-  ) {
+  public static to(name: string, target: string, duration: number = 200, vars: any, complete?: () => void) {
     duration = duration === 0 ? 1 : duration;
 
     // gsap.TweenLite.killTweensOf(target, true);
 
     setTimeout(() => {
       // Be sure DOM was created
-      gsap.TweenLite.to(target, duration / 1000, vars).eventCallback(
-        "onComplete",
-        () => {
-          if (complete) {
-            complete();
-          }
-          // console.log(
-          //   "[Animation] %s animation completed. (target=%s, duration=%d)",
-          //   name,
-          //   target,
-          //   duration
-          // );
+      gsap.TweenLite.to(target, duration / 1000, vars).eventCallback("onComplete", () => {
+        if (complete) {
+          complete();
         }
-      );
+        // console.log(
+        //   "[Animation] %s animation completed. (target=%s, duration=%d)",
+        //   name,
+        //   target,
+        //   duration
+        // );
+      });
     }, 0);
   }
 }
