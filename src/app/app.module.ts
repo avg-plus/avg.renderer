a4f5707529bac6ce27c28d1518a7962af5023089
AngryPowman <138001655@qq.com>
2019-09-02 03:56:10 +0800

    refactor: upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule, enableProdMode } from "@angular/core";
 import { FormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
+// import { HttpModule } from "@angular/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from "./app.component";
