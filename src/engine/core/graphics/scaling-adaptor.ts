e1646aceb66ffc8d456ed834f099bf00705013b0
HTTdesu <592421947@qq.com>
2019-12-11 17:26:27 +0800

    solving conflict

--- src/engine/core/graphics/scaling-adaptor.ts
+++ src/engine/core/graphics/scaling-adaptor.ts
@@ -27,34 +27,36 @@ export class ScalingAdaptor {
     GameWorld.app.stage = this.postStage;
   }
 
-    resize(width: number, height: number, method: ScalingMethod = ScalingMethod.AUTO) {
-        let baseWidth = GameWorld.worldWidth;
-        let baseHeight = GameWorld.worldHeight;
+  resize(width: number, height: number, method: ScalingMethod = ScalingMethod.AUTO) {
+    let baseWidth = GameWorld.worldWidth;
+    let baseHeight = GameWorld.worldHeight;
 
-		if(method === ScalingMethod.AUTO) {
-			if(width / height > baseWidth / baseHeight) {
-				method = ScalingMethod.ACCORDING_TO_HEIGHT;
-			} else {
-				method = ScalingMethod.ACCORDING_TO_WIDTH;
-			}
-		}
-
-		let edgeRatio = 0;
-		switch(method) {
-			case ScalingMethod.ACCORDING_TO_WIDTH:
-                edgeRatio = width / baseWidth;
-                this.postStage.scale = new PIXI.Point(edgeRatio, edgeRatio);
-                this.postStage.y = (height - this.postStage.height) / 2.0;
-				break;
-			case ScalingMethod.ACCORDING_TO_HEIGHT:
-                edgeRatio = height / baseHeight;
-                this.postStage.scale = new PIXI.Point(edgeRatio, edgeRatio);
-                this.postStage.x = (width - this.postStage.width) / 2.0;
-				break;
-			default:
-				// TODO: throw exception
-        }
+    if(method === ScalingMethod.AUTO) {
+      if(width / height > baseWidth / baseHeight) {
+        method = ScalingMethod.ACCORDING_TO_HEIGHT;
+      } else {
+        method = ScalingMethod.ACCORDING_TO_WIDTH;
+      }
     }
+
+    let edgeRatio = 0;
+    switch(method) {
+      case ScalingMethod.ACCORDING_TO_WIDTH:
+        edgeRatio = width / baseWidth;
+        this.postStage.scale = new PIXI.Point(edgeRatio, edgeRatio);
+        this.postStage.x = 0.0;
+        this.postStage.y = (height - this.postStage.height) / 2.0;
+        break;
+      case ScalingMethod.ACCORDING_TO_HEIGHT:
+        edgeRatio = height / baseHeight;
+        this.postStage.scale = new PIXI.Point(edgeRatio, edgeRatio);
+        this.postStage.x = (width - this.postStage.width) / 2.0;
+        this.postStage.y = 0.0;
+        break;
+      default:
+        // TODO: throw exception
+    }
+  }
 }
 
 export const enum ScalingMethod {
