d22f9492f709b1647b550e9fd630d4fd34d8a729
AngryPowman <powmanx2@gmail.com>
2020-08-23 04:17:59 +0800

    feat(filter): add tilt shift effects

--- src/engine/core/graphics/filters/tilt-shift.ts
+++ src/engine/core/graphics/filters/tilt-shift.ts
@@ -1,12 +1,36 @@
 import * as PIXI from "pixi.js";
 import * as ExtraFilters from "pixi-filters";
-import { FilterBase } from "./filter-base";
+import { FilterBase, IFilterArgs } from "./filter-base";
+import { Sprite } from "../sprite";
+
+interface ITiltShiftFilterArgs extends IFilterArgs {
+  blur: number;
+  end: PIXI.Point;
+  gradientBlur: number;
+  start: PIXI.Point;
+}
 
 class TiltShiftFilter extends FilterBase {
   name: "TiltShiftFilter";
 
-  public instance() {
-    return new ExtraFilters.TiltShiftFilter();
+  public instance(parent: Sprite, args: ITiltShiftFilterArgs) {
+    const data = Object.assign(
+      {},
+      {
+        blur: 100,
+        gradientBlur: 500,
+        start: null,
+        end:null,
+      },
+      args
+    );
+
+    return new ExtraFilters.TiltShiftFilter(
+      data.blur,
+      data.gradientBlur,
+      data.start,
+      data.end
+    );
   }
 
   public validate(folder) {}
