dac9e92a6477feb3ddc96e053d309fd52d08d537
AngryPowman <powmanx2@gmail.com>
2020-08-31 00:30:31 +0800

    feat(API): remove widget text

--- src/engine/scripting/exports/widget.ts
+++ src/engine/scripting/exports/widget.ts
@@ -1,83 +1,25 @@
 import { AVGSpriteRenderer } from "../../data/sprite-renderer";
 import { APIExport, AVGExportedAPI } from "./avg-exported-api";
 import { Subtitle } from "../../data/screen-subtitle";
-import {
-  APIScreenSubtitle,
-  ScreenSubtitleResult
-} from "../api/api-screen-subtitle";
-import {
-  ScreenPosition,
-  WidgetAnimation,
-  ScreenWidgetAnimation,
-  WidgetAnimation_FadeInOptions,
-  WidgetAnimation_HideOptions
-} from "../../data/screen-widget";
+import { APIScreenSubtitle } from "../api/api-screen-subtitle";
 import { APIManager } from "../api-manager";
 import { OP } from "../../const/op";
 import { ScreenImage } from "../../data/screen-image";
 import { APIScreenImage } from "../api/api-screen-image";
 import { ResourceData } from "../../data/resource-data";
 import { ResourcePath } from "../../core/resource";
-import { paramCompatible } from "../../core/utils";
 import { APIHtmlWidget } from "../api/api-html-widget";
 import { EngineUtils } from "../../core/engine-utils";
 import { Sandbox } from "../../core/sandbox";
 import { SpriteAnimationMacro } from "engine/core/graphics/sprite-animate-director";
-import { TransformConverter } from "engine/core/transform-converter";
 import { ScreenWidgetHtml } from "engine/data/screen-widget-html";
-import { SpriteWidgetManager } from 'engine/core/graphics/sprite-widget-manager';
+import { SpriteWidgetManager } from "engine/core/graphics/sprite-widget-manager";
+
+import { GameWorld } from "engine/core/graphics/world";
 
 @APIExport("widget", EngineAPI_Widget)
 export class EngineAPI_Widget extends AVGExportedAPI {
-  // public static async text(id: string, text: string, options?: Subtitle, isAsync: boolean = false) {
-  //   let model = new APIScreenSubtitle();
-  //   model.isAsync = isAsync;
-  //   model.data = new Subtitle();
-  //   Object.assign(model.data, options);
-
-  //   // model.data.id = "Text_" + IDGenerator.generate();
-  //   model.data.name = EngineUtils.makeWidgetID(id);
-
-  //   model.data.text = text;
-  //   model.data.position = options.position || ScreenPosition.Center;
-  //   // model.data.animation = options.animation || new WidgetAnimation();
-  //   // model.data.animation.name = model.data.animation.name || ScreenWidgetAnimation.Enter_Appear;
-  //   // model.data.animation.options = model.data.animation.options || new WidgetAnimation_FadeInOptions();
-
-  //   // if (!model.data) {
-  //   //     model.data.animation.name = ScreenWidgetAnimation.Enter_Appear;
-  //   // }
-
-  //   // paramCompatible<APIScreenSubtitle, Subtitle>(model, options);
-
-  //   // 跳过模式处理，跳过不执行动画
-  //   if (Sandbox.isSkipMode && Sandbox.skipOptions.widgets === true) {
-  //     // model.data.animation.options.duration = 0;
-  //   }
-
-  //   return <ScreenSubtitleResult>(
-  //     await APIManager.Instance.getImpl(APIScreenSubtitle.name, OP.ShowTextWidget).runner(<APIScreenSubtitle>model)
-  //   );
-  // }
-
-  // public static async animateText(id: string) {
-  //   let model = new APIScreenSubtitle();
-  //   model.data.name = EngineUtils.makeWidgetID(id);
-
-  //   const proxy = APIManager.Instance.getImpl(APIScreenSubtitle.name, OP.AnimateTextWidget);
-  //   proxy && (await proxy.runner(<APIScreenSubtitle>model));
-  // }
-
-  // public static async updateText(id: string, text: string) {
-  //   let model = new APIScreenSubtitle();
-  //   model.data.name = EngineUtils.makeWidgetID(id);
-  //   model.data.text = text;
-
-  //   const proxy = APIManager.Instance.getImpl(APIScreenSubtitle.name, OP.UpdateTextWidget);
-
-  //   proxy && (await proxy.runner(<APIScreenSubtitle>model));
-  // }
-
+  
   public static async image(
     name: string,
     filename: string,
@@ -235,5 +177,5 @@ export class EngineAPI_Widget extends AVGExportedAPI {
     proxy && (await proxy.runner(<APIHtmlWidget>model));
   }
 
-  public static updateHTMLWidgetOption(name: string) { }
+  public static updateHTMLWidgetOption() {}
 }
